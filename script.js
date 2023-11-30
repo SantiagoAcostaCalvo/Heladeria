f430adcb24d543998333ca7b74cbe97478315672
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2023-11-29 20:50:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function agregarAlCarrito(productId) {
+    // Aquí puedes implementar la lógica para agregar productos al carrito
+    // Por ejemplo, puedes utilizar Local Storage para almacenar los productos seleccionados
+    alert("Producto agregado al carrito. ¡Disfruta de tu helado!");
+}
